140c0e974a4060e5bf2e79f5bab58c3e9506ac47
mnjrupp <mnjrupp@hotmail.com>
2022-05-09 15:19:53 -0500

    1 file modified

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {LoanDataService} from '../shared/loan-data.service';
 import {PricingModel} from '../pricing-model';
+import Patronage from '../../assets/PatHist.json';
 
 @Component({
   selector: 'app-customer',
@@ -9,7 +10,10 @@ import {PricingModel} from '../pricing-model';
 })
 export class CustomerComponent implements OnInit {
   pricing:PricingModel;
-  constructor(private loanservice:LoanDataService) { }
+  patronage:any;
+  constructor(private loanservice:LoanDataService) { 
+    this.patronage=Patronage;
+  }
 
   ngOnInit() {
     this.loanservice.cast.subscribe(data=>this.pricing = data);
