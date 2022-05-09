650eb7f411801e6710ab6b7680191f6583261d8e
mnjrupp <mnjrupp@hotmail.com>
2022-05-09 10:05:51 -0500

    1 file modified

--- src/app/loanofficer/loanofficer.component.ts
+++ src/app/loanofficer/loanofficer.component.ts
@@ -209,8 +209,8 @@ export class LoanOfficerComponent implements OnInit {
     var estIntRate2 = (recomrate2+(parseFloat(this.pricing.Variance2) || 0))
     var estIntRate3 = (recomrate3+(parseFloat(this.pricing.Variance3) || 0))
     var payment1 = -1*this.build1stPaymnt(estIntRate1,(this.pricing.AmorTerm1*this.pricing.paymentfreq),this.unformatNumber(this.pricing.loanAmnt))
-    var payment2 = this.build1stPaymnt(estIntRate2,(this.pricing.AmorTerm2*this.pricing.paymentfreq),this.unformatNumber(this.pricing.loanAmnt))
-    var payment3 = this.build1stPaymnt(estIntRate3,(this.pricing.AmorTerm3*this.pricing.paymentfreq),this.unformatNumber(this.pricing.loanAmnt))
+    var payment2 = -1*this.build1stPaymnt(estIntRate2,(this.pricing.AmorTerm2*this.pricing.paymentfreq),this.unformatNumber(this.pricing.loanAmnt))
+    var payment3 = -1*this.build1stPaymnt(estIntRate3,(this.pricing.AmorTerm3*this.pricing.paymentfreq),this.unformatNumber(this.pricing.loanAmnt))
      
     //console.log('Loan Officer Variance 1 ',this.pricing.Variance1)
     //console.log('Recommended Spread 1 ',recomspread1);
