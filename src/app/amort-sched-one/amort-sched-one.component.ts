49ffefdcdfa13c82734208b1294b7fed652c6282
mnjrupp <mnjrupp@hotmail.com>
2022-05-12 13:32:44 -0500

    2 files modified

--- src/app/amort-sched-one/amort-sched-one.component.ts
+++ src/app/amort-sched-one/amort-sched-one.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {LoanDataService} from '../shared/loan-data.service';
+import {PricingModel} from '../pricing-model';
 
 @Component({
   selector: 'app-amort-sched-one',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./amort-sched-one.component.css']
 })
 export class AmortSchedOneComponent implements OnInit {
+  pricing:PricingModel;
 
-  constructor() { }
+
+  constructor(private loanservice:LoanDataService) {
+
+   }
 
   ngOnInit() {
+    this.loanservice.cast.subscribe(data=> this.pricing = data);
   }
 
 }
