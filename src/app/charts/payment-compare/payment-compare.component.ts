53676cee1a120d6a476e2c386b956081de3e2c29
mnjrupp <mnjrupp@hotmail.com>
2022-05-31 11:44:12 -0500

    3 files modified

--- src/app/charts/payment-compare/payment-compare.component.ts
+++ src/app/charts/payment-compare/payment-compare.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Chart } from 'chart.js';
 @Component({
   selector: 'app-charts-payment-compare',
   templateUrl: './payment-compare.component.html',
@@ -7,9 +7,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentCompareComponent implements OnInit {
 
+  dataArray: any = [];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    let data: any,
+      options: any,
+      chart: any,
+      ctx: any = document.getElementById('areaChart') as HTMLElement;
+
+    // Stackblitz no longer supports local json files.
+    // Uncomment below and use import at top.
+    // Replace datasets with this.dataArray
+
+    // for (let key in chartData.items) {
+    //   if (chartData.items.hasOwnProperty(key)) {
+    //     this.dataArray.push(chartData.items[key]);
+    //   }
+    // }
+
+    data = {
+      labels: ['Apples', 'Oranges', 'Mixed Fruit'],
+      datasets: [
+        {
+          label: 'Apples',
+          data: [0, 50, 45, 100],
+          backgroundColor: 'rgba(40,125,200,.5)',
+          borderColor: 'rgb(40,100,200)',
+          fill: false,
+          lineTension: 0,
+          radius: 5,
+        },
+        {
+          label: 'Oranges',
+          data: [30, 90, 111, 20],
+          backgroundColor: 'rgba(75,10,125,.5)',
+          borderColor: 'rgb(75,10,125)',
+          fill: false,
+          lineTension: 0.2,
+          radius: 5,
+        },
+      ],
+    };
+
+    options = {
+      responsive: true,
+      maintainAspectRatio: false,
+      title: {
+        display: true,
+        position: 'top',
+        text: 'Apples to Oranges',
+        fontSize: 12,
+        fontColor: '#666',
+      },
+      legend: {
+        display: true,
+        position: 'bottom',
+        labels: {
+          fontColor: '#999',
+          fontSize: 14,
+        },
+      },
+    };
+
+    chart = new Chart(ctx, {
+      type: 'line',
+      data: data,
+      options: options,
+    });
   }
 
 }
