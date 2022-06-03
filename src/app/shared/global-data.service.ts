23fc1dc8b2a02eaa10512bf6621157c4e16e7503
mnjrupp <mnjrupp@hotmail.com>
2022-06-03 11:35:26 -0500

    1 file modified

--- src/app/shared/global-data.service.ts
+++ src/app/shared/global-data.service.ts
@@ -3,7 +3,15 @@ import {BehaviorSubject} from 'rxjs';
 
 @Injectable()
 export class GlobalDataService {
+  // Set up default boolean variables for page display
   isShown = true;
+  isShownCustomer = false;
+  isShownAmort1 = false;
+  isShownAmort2 = false;
+  isShownAmort3 = false;
+  isShownChartInt = false;
+  isShownChartPay = false;
+  isShownCalc = false;
   private toggleMessage=new BehaviorSubject(this.isShown);
   currentToggleMessage = this.toggleMessage.asObservable();
 
