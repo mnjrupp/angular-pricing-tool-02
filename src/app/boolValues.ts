9b41e455f542bea5a60059be42dd689d87dcc657
mnjrupp <mnjrupp@hotmail.com>
2022-06-03 14:57:39 -0500

    1 file modified, 1 added

---
+++ src/app/boolValues.ts
@@ -0,0 +1,11 @@
+export interface BoolValues {
+
+  isShown: boolean,
+  isShownCustomer: boolean,
+  isShownAmort1: boolean,
+  isShownAmort2:boolean,
+  isShownAmort3: boolean,
+  isShownChartInt:boolean,
+  isShownChartPay:boolean,
+  isShownCalc:boolean,
+}
--- src/app/shared/global-data.service.ts
+++ src/app/shared/global-data.service.ts
@@ -4,20 +4,25 @@ import {BehaviorSubject} from 'rxjs';
 @Injectable()
 export class GlobalDataService {
   // Set up default boolean variables for page display
-  isShown = true;
-  isShownCustomer = false;
-  isShownAmort1 = false;
-  isShownAmort2 = false;
-  isShownAmort3 = false;
-  isShownChartInt = false;
-  isShownChartPay = false;
-  isShownCalc = false;
-  private toggleMessage=new BehaviorSubject(this.isShown);
+  
+  
+
+  boolValues = {
+  isShown: true,
+  isShownCustomer: false,
+  isShownAmort1: false,
+  isShownAmort2:false,
+  isShownAmort3: false,
+  isShownChartInt:false,
+  isShownChartPay:false,
+  isShownCalc:false,
+  }
+  private toggleMessage=new BehaviorSubject(this.boolValues);
   currentToggleMessage = this.toggleMessage.asObservable();
 
   constructor() { }
   
-  toggleisShown(message:boolean){
+  toggleisShown(message){
     this.toggleMessage.next(message);
   }
 
