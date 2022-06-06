d077fa643c8cbec0e94b4602a1a52a378d67a2af
mnjrupp <mnjrupp@hotmail.com>
2022-06-06 13:47:15 -0500

    2 files modified

--- src/app/charts/payment-compare/payment-compare.component.ts
+++ src/app/charts/payment-compare/payment-compare.component.ts
@@ -26,76 +26,84 @@ export class PaymentCompareComponent implements OnInit {
       this.payments2 = this.paymentservice.buildpayments(this.pricing,2);
       //console.log('payments2 ',this.payments2['cumpayment'])
       this.payments3 = this.paymentservice.buildpayments(this.pricing,3);
-     
+      this.buildChartInterface();
     });
   }
 
   ngAfterViewInit() {
-    let data: any,
-      options: any,
-      chart: any,
-      ctx: any = document.getElementById('areaChart') as HTMLElement;
-
-   
-
-    data = {
-      labels: ['Scenario 1', 'Scenario 2', 'Scenario 3'],
-      datasets: [
-        {
-          label: this.pricing.AmorType1,
-          data: this.payments1.map(d=>d.cumpayment),
-          backgroundColor: 'rgba(40,125,200,.5)',
-          borderColor: 'rgb(40,100,200)',
-          fill: false,
-          lineTension: 0,
-          radius: 2,
-        },
-        {
-          label: this.pricing.AmorType2,
-          data:this.payments2.map(d=>d.cumpayment),
-          backgroundColor: 'rgba(138,11,26,.5)',
-          borderColor: 'rgb(138,11,26)',
-          fill: false,
-          lineTension: 0.2,
-          radius: 2,
-        },
-        {
-          label: this.pricing.AmorType3,
-          data: this.payments3.map(d=>d.cumpayment),
-          backgroundColor: 'rgba(11,138,93,.5)',
-          borderColor: 'rgb(11,138,93)',
-          fill: false,
-          lineTension: 0.2,
-          radius: 2,
-        },
-      ],
-    };
-
-    options = {
-      responsive: true,
-      maintainAspectRatio: false,
-      title: {
-        display: true,
-        position: 'top',
-        text: 'Apples to Oranges',
-        fontSize: 12,
-        fontColor: '#666',
-      },
-      legend: {
-        display: true,
-        position: 'bottom',
-        labels: {
-          fontColor: '#999',
-          fontSize: 14,
-        },
-      },
-    };
-
-    chart = new Chart(ctx, {
-      type: 'line',
-      data: data,
-      options: options,
-    });
+   this.buildChartInterface();
   }
 
+  buildChartInterface(){
+    let data: any,
+    options: any,
+    chart: any,
+    canvas:any = document.getElementById('areaChart') as HTMLElement;
+    console.log('canvas object ',canvas);
+    canvas.remove();
+    let division: any = document.getElementById('myChart') as HTMLElement;
+        division.append('<canvas id="areaChart"></canvas>');
+    let ctx: any = document.getElementById('areaChart') as HTMLElement;
+    //ctx.destroy();
+ 
+
+  data = {
+    labels: ['Scenario 1', 'Scenario 2', 'Scenario 3'],
+    datasets: [
+      {
+        label: this.pricing.AmorType1,
+        data: this.payments1.map(d=>d.cumpayment),
+        backgroundColor: 'rgba(40,125,200,.5)',
+        borderColor: 'rgb(40,100,200)',
+        fill: false,
+        lineTension: 0,
+        radius: 2,
+      },
+      {
+        label: this.pricing.AmorType2,
+        data:this.payments2.map(d=>d.cumpayment),
+        backgroundColor: 'rgba(138,11,26,.5)',
+        borderColor: 'rgb(138,11,26)',
+        fill: false,
+        lineTension: 0.2,
+        radius: 2,
+      },
+      {
+        label: this.pricing.AmorType3,
+        data: this.payments3.map(d=>d.cumpayment),
+        backgroundColor: 'rgba(11,138,93,.5)',
+        borderColor: 'rgb(11,138,93)',
+        fill: false,
+        lineTension: 0.2,
+        radius: 2,
+      },
+    ],
+  };
+
+  options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    title: {
+      display: true,
+      position: 'top',
+      text: 'Apples to Oranges',
+      fontSize: 12,
+      fontColor: '#666',
+    },
+    legend: {
+      display: true,
+      position: 'bottom',
+      labels: {
+        fontColor: '#999',
+        fontSize: 14,
+      },
+    },
+  };
+
+  chart = new Chart(ctx, {
+    type: 'line',
+    data: data,
+    options: options,
+  });
 }
+  }
