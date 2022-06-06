a703283111268254f63a97a5eb4beed1a1317f90
mnjrupp <mnjrupp@hotmail.com>
2022-06-06 11:22:45 -0500

    1 file modified

--- src/app/charts/payment-compare/payment-compare.component.ts
+++ src/app/charts/payment-compare/payment-compare.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart } from 'chart.js';
 import {PaymentModel} from '../../payment-model';
+import { PricingModel } from '../../pricing-model';
+import { LoanDataService } from '../../shared/loan-data.service';
+import {PaymentDataService} from '../../shared/payment-data.service';
 @Component({
   selector: 'app-charts-payment-compare',
   templateUrl: './payment-compare.component.html',
@@ -9,10 +12,18 @@ import {PaymentModel} from '../../payment-model';
 export class PaymentCompareComponent implements OnInit {
 
   dataArray: any = [];
-
-  constructor() { }
+  pricing: PricingModel;
+  payments:any[] = []
+  constructor(private loanservice: LoanDataService,private paymentservice:PaymentDataService) { }
 
   ngOnInit() {
+    this.loanservice.cast.subscribe((data) => {
+      this.pricing = data;
+      this.payments[0] = this.paymentservice.buildpayments(this.pricing,1);
+      this.payments[1] = this.paymentservice.buildpayments(this.pricing,2);
+      this.payments[2] = this.paymentservice.buildpayments(this.pricing,3);
+     
+    });
   }
 
   ngAfterViewInit() {
@@ -21,21 +32,13 @@ export class PaymentCompareComponent implements OnInit {
       chart: any,
       ctx: any = document.getElementById('areaChart') as HTMLElement;
 
-    // Stackblitz no longer supports local json files.
-    // Uncomment below and use import at top.
-    // Replace datasets with this.dataArray
-
-    // for (let key in chartData.items) {
-    //   if (chartData.items.hasOwnProperty(key)) {
-    //     this.dataArray.push(chartData.items[key]);
-    //   }
-    // }
+   
 
     data = {
       labels: ['Scenario 1', 'Scenario 2', 'Scenario 3'],
       datasets: [
         {
-          label: 'Scenario 1',
+          label: this.pricing.AmorType1,
           data: [0, 50, 45, 100],
           backgroundColor: 'rgba(40,125,200,.5)',
           borderColor: 'rgb(40,100,200)',
@@ -44,7 +47,7 @@ export class PaymentCompareComponent implements OnInit {
           radius: 2,
         },
         {
-          label: 'Scenario 2',
+          label: this.pricing.AmorType2,
           data: [30, 90, 111, 20],
           backgroundColor: 'rgba(138,11,26,.5)',
           borderColor: 'rgb(138,11,26)',
@@ -53,7 +56,7 @@ export class PaymentCompareComponent implements OnInit {
           radius: 2,
         },
         {
-          label: 'Scenario 3',
+          label: this.pricing.AmorType3,
           data: [20, 70, 75, 80],
           backgroundColor: 'rgba(11,138,93,.5)',
           borderColor: 'rgb(11,138,93)',
